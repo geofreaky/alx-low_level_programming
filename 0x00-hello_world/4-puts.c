258631cf8e3e577496c7f5f36fefc488054f3a28
geofreaky <geoazi14@gmail.com>
2022-06-04 21:18:26 -0700

    dail

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * * main - entry point
+ * *
+ * * Return: 0 number
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}}
