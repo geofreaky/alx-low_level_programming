877ff03d84e2adc9b5437ab8267c80a59b1ce7a9
geofreaky <geoazi14@gmail.com>
2022-06-05 17:53:34 -0700

    please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * * main - entry point
+ * *
+ * * Return: 0 number
+ * */
 int main(void)
-{ puts("Programming is like building a multilingual puzzle\n");
-       	return (0); }
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
