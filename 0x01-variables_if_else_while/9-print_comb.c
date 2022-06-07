15b29d39ce1d4404fd5417364c02c99275f45a95
geofreaky <geoazi14@gmail.com>
2022-06-06 18:50:35 -0700

    falliate

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: end program
+ */
+int main(void)
+{	int x = '0';
+
+	while (x <= '9')
+	{	putchar(x);
+	}
+	if (x != '9')
+	{	putchar(',');
+		putchar(' ');
+	}
+	x++;
+}
