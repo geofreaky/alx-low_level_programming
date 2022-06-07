aa754af94ca0b9bb2f1536a2ba0139e02d5d1518
geofreaky <geoazi14@gmail.com>
2022-06-06 17:11:21 -0700

    push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: End the program
+ */
+int main(void)
+{	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	printf("%c", letter);
+	printf("\n");
+	return (0);
+}
