f7ab1ecc0618d606e205169fd0c825cd9d6a0b39
geofreaky <geoazi14@gmail.com>
2022-06-08 14:41:04 -0700

    takau

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,13 +5,13 @@
 void jack_bauer(void)
 {
 	int hour, h1, h2, min, m1, m2;
+
 	for (hour = 0; hour <= 23; ++hour)
 	{
 		h1 = hour / 10;
 		h2 = hour % 10;
 		for (min = 0; min < 60; ++min)
-		{
-			m1 = min / 10;
+		{	m1 = min / 10;
 			m2 = min % 10;
 			_putchar(h1 + '0');
 			_putchar(h2 + '0');
