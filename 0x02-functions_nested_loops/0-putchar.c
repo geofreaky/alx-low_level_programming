6e7b5638ebec76be8b380b6bfe9502f6f3061a82
geofreaky <geoazi14@gmail.com>
2022-06-07 20:55:08 -0700

    photon

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,15 @@
  * Return: End program
  */
 int main(void)
-{	char string[8] = "putchar";
+{
+	char string[8] = "_putchar";
 	int n = 0;
+
 	while (n < 8)
 	{	_putchar(string[n]);
 		++n;
 	}
+
 	_putchar(10);
 	return (0);
 }
