efc1cc7cfe875ad7da965f2f5dd48f576739af38
geofreaky <geoazi14@gmail.com>
2022-06-10 18:49:05 -0700

    hello

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,10 +3,12 @@
  * print_most_numbers - function
  */
 void print_most_numbers(void)
-{	int n = 48;
+{
+	int n = 48;
 
 	while (n < 58)
-	{	if (n != 50 && n != 52)
+	{
+		if (n != 50 && n != 52)
 			_putchar(n);
 		n++;
 	}
