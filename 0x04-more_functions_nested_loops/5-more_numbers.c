8f929ba8486eb34fd81e5a464492186a92c2bf7e
geofreaky <geoazi14@gmail.com>
2022-06-10 18:52:00 -0700

    hello

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,14 +3,18 @@
  * more_numbers - function
  */
 void more_numbers(void)
-{	int n = 48;
+{
+	int n = 48;
 	int l = 0;
 	int reset = 0;
 
 	while (l < 10)
-	{	while (n <= 62)
-		{	if (n > 57)
-			{	reset = 10;
+	{
+		while (n <= 62)
+		{
+			if (n > 57)
+			{
+				reset = 10;
 				_putchar(49);
 			}
 			_putchar(n - reset);
