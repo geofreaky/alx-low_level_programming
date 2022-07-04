cb9641099db2583ae101038525b9f2b5eb3b05c4
Bigteddy <102831977+geofreaky@users.noreply.github.com>
2022-07-04 11:05:32 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,14 +10,11 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == 0 || height == 0)u
-
-		for (i = 0; i < height; i++)
-			return;
+	if (grid == 0 || height == 0)
+		return;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
 
 	free(grid);
-
 }
