db76478d64e04aa77e4668d830c72415e8cc6877
Bigteddy <102831977+geofreaky@users.noreply.github.com>
2022-07-16 05:14:01 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,28 @@
 #include "lists.h"
 /**
- * add_node_end - function that adds a new node
- * at the end of a list_t list.
+ * add_node - function that adds a new node
+ * at the beginning of a list_t list.
  *
- * @head: Parameter that point to de head of the
- * linked list
- * @str: Parameter with the string
+ * @head: Parameter with the head node
+ * @str: parameter with the element str in
+ * the structure
  *
- * Return: the address of the new element,
- * or NULL if it failed
+ * Return: Return the head of the linked list
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new, *temp;
+	list_t *new;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = NULL;
+	new->next = *head;
+	*head = new;
 
-	temp = *head;
-	if (*head == NULL)
-	{
-		*head = new;
-	}
-	else
-	{
-		while (temp->next)
-		{
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
-
-	return (new);
+	return (*head);
 }
