0f624c850e86d042fe74bc52a35d0553d70dbcfc
geofreaky <geoazi14@gmail.com>
2022-07-20 22:16:50 -0700

    late work

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,4 @@
-include "lists.h"
+#include "lists.h"
 /**
  * add_nodeint - function that adds a new node at the
  * beginning of a listint_t list.
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,4 @@
-include "lists.h"
+#include "lists.h"
 /**
  * add_nodeint_end - Function that adds a new node at the end
  * of a listint_t list.
